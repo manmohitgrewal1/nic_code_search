2ed7364c80acaa2f977224ed2de8d7bb810ca663
manmohit <manmohitgrewal1@gmail.com>
2021-12-01 23:01:18 +0530

    cleared plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,5 @@
  */
 
 module.exports = {
-  siteMetadata: {
-    title: `NIC code search online`,
-    description: `NIC code for your business to obtain Udyog Aadhar. Find section and division of a incorporated company.`,
-    author: `@crustdata`,
-    keywords: `NIC code, CIN code, MCA cin number, Alternative data, crustdata, crustdata alternative data`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [],
 }
